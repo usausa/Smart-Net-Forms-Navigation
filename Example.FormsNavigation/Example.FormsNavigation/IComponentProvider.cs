3469cecc3dfbfd6d8e13f52595e7041f9b19df3b
usausa <machi.pon@gmail.com>
2017-02-19 18:54:40 +0900

    Add resolver project

---
+++ Example.FormsNavigation/Example.FormsNavigation/IComponentProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.FormsNavigation
+{
+    using Smart.Resolver;
+
+    public interface IComponentProvider
+    {
+        void RegisterComponents(ResolverConfig config);
+    }
+}
---
+++ Smart.Forms.Navigation.Resolver/Forms/Navigation/Components/SmartResolverActivator.cs
@@ -0,0 +1,22 @@
+﻿namespace Smart.Forms.Navigation.Components
+{
+    using System;
+
+    using Smart.Resolver;
+
+    public class SmartResolverActivator : IActivator
+    {
+        private readonly IResolver resolver;
+
+        public SmartResolverActivator(IResolver resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public object Get(Type type)
+        {
+            // TODO IE?
+            return resolver.Get(type);
+        }
+    }
+}
