62660fd600ab2c6bda5bf708844147afb2fd69b6
usausa <machi.pon@gmail.com>
2017-05-16 21:59:07 +0900

    Version 0.8.0

---
+++ Smart.Forms.Navigation/Forms/Navigation/Components/StandardConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Smart.Forms.Navigation.Components
+{
+    using System;
+
+    using Smart.Converter;
+
+    public class StandardConverter : IConverter
+    {
+        private readonly IObjectConverter converter;
+
+        public StandardConverter()
+            : this(ObjectConverter.Default)
+        {
+        }
+
+        public StandardConverter(IObjectConverter converter)
+        {
+            this.converter = converter;
+        }
+
+        public object Convert(object value, Type type)
+        {
+            return converter.Convert(value, type);
+        }
+    }
+}
---
+++ Smart.Forms.Navigation/Forms/Navigation/PathPageResolver.cs
@@ -0,0 +1,51 @@
+﻿namespace Smart.Forms.Navigation
+{
+    using System;
+    using System.Reflection;
+
+    using Xamarin.Forms;
+
+    public class PathPageResolver : IPageResolver
+    {
+        private const string Separator = "/";
+
+        private string @namespace;
+
+        public string Namespace
+        {
+            get => @namespace ?? (@namespace = String.Concat(Application.Current.GetType().Namespace, ".Pages"));
+            set => @namespace = value;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Framework only")]
+        public string NormarizeName(string current, string name)
+        {
+            if (name.StartsWith(Separator, StringComparison.OrdinalIgnoreCase))
+            {
+                return name;
+            }
+
+            if (!String.IsNullOrEmpty(current))
+            {
+                var index = current.LastIndexOf(Separator, StringComparison.OrdinalIgnoreCase);
+                return String.Concat(current.Substring(0, index + 1), name);
+            }
+
+            return String.Concat(Separator, name);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Framework only")]
+        public Type ResolveType(string name)
+        {
+            var typeName = String.Concat(Namespace, name.Replace(Separator, "."));
+            return Application.Current.GetType().GetTypeInfo().Assembly.GetType(typeName);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Framework only")]
+        public string ResolveName(Type type)
+        {
+            var name = type.FullName.Substring(Namespace.Length);
+            return name.Replace(".", Separator);
+        }
+    }
+}
